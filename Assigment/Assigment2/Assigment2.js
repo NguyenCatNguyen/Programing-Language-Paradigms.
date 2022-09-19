34e46881c0456c6fc912620377a28315fd4fec46
NguyenCatNguyen <catngn11@gmail.com>
2022-09-19 18:55:24 -0500

    final product

--- Assigment/Assigment2/Assigment2.js
+++ Assigment/Assigment2/Assigment2.js
@@ -1,49 +1,7 @@
 /*
-
-
-
-The standard JavaScript environment provides another data structure called Set.
-• A set holds a collection of values (from Set Theory – EECS 210, see Sets & Set
-Operations.pptx for a review).
-• A value can be part of a set only once—adding it again doesn’t have any effect.
-• Write a class called Group (since Set is already taken).
-• Group should have 6 methods: add, delete, has, union, intersect, and difference.
-• Its constructor creates an empty group.
-• add adds a value to the group (but only if it isn’t already a member)
-• delete removes its argument from the group (if it was a member)
-• has returns a Boolean value indicating whether its argument is a member of the
-group
-• union returns the union of the group and the argument, which should be another
-group: this ∪ argument
-• intersection returns the intersection of the group and the argument, which should
-be another group: this ∩ argument
-• difference returns the difference of the group and the argument, which should be
-another group: this - argument
-• Test your Group class with the following code:
-let group1 = new Group();
-let group2 = new Group();
-group1.add(1);
-group1.add(2);
-group1.add(3);
-console.log(group1);
-group2.add(2);
-group2.add(3);
-group2.add(5);
-group2.add(2);
-console.log(group2);
-console.log(group1.has(5));
-console.log(group2.has(3));
-console.log(group1.union(group2));
-console.log(group1.intersection(group2));
-console.log(group1.difference(group2));
-group1.delete(1);
-console.log(group1);
-group2.delete(1);
-console.log(group2);
-• Provide comments that explain what each line of code is doing. You may
-comment each line of code (using //) and/or provide a multi-line comment
-(using /* and
-
+Authour: Nguyen Cat Nguyen
+KUID: 3077463
+Date: Monday Sep 19, 2022
 */
 
 
@@ -55,72 +13,103 @@ class Group {
         this.group = [];
         //Create empty group
     }
-    //Create add method, only add if not already a member.
+    //Create add method, only add value if it isn't already a member.
     add(value) {
-        if(!(value in this.group)) {
-            //Check if value is in group
+        if(!this.has(value)) {
             this.group.push(value);
-            //Add value to group
         }
     }
-    //Create delete method, remove argument from group if it was a member.
+    //Create delete method, remove value from group if it was a member.
     delete(value) {
-        if(value in this.group) {
+        if(this.has(value)) {
             //Check if value is in group
-            this.group.pop(this.value);
+            this.group.splice(this.group.indexOf(value), 1);
             //Remove value from group
         }
-    } 
-    //Create has method, return boolean value indicating if argument is a member of group.
+        }
+
+    //Create has method, return true if value in group, false if not.
     has(value) {
-        if(value in this.group) {
+        if(this.group.includes(value)) {
             //Check if value is in group
             return true;
-            //Return true if value is in group
-        }
-        else {
+        } else {
             return false;
-            //Return false if value is not in group
-        }
+        } 
     }
     //Create intersection method, check if value in this group is in another group. Then return the common of the two groups in a new group.
     intersection(value) {
         let newGroup = new Group();
         //Create new group
         for(let i = 0; i < this.group.length; i++) {
-            //Loop through group
-            if(this.group[i] in value.group) {
-                //Check if value in this group is in another group
+            if(value.has(this.group[i])) {
                 newGroup.add(this.group[i]);
                 //Add value to new group
             }
         }
         return newGroup;
-        //Return new group
+        //return new group
     }
-    //Create union method, check if value in this group is in another group. Then return this group with the difference from the other group to the new group.
+    //Create union method, check if value in this group is in another group. Then return the union of the two groups in a new group.
     union(value) {
         let newGroup = new Group();
         //Create new group
         for(let i = 0; i < this.group.length; i++) {
-            //Loop through group
-            if(this.group[i] in value.group) {
-                //Check if value in this group is in another group
+            newGroup.add(this.group[i]);
+            //Add value to new group
+        }
+        for(let i = 0; i < value.group.length; i++) {
+            newGroup.add(value.group[i]);
+            //Add value to new group
+        }
+        return newGroup;
+    }
+    //Create difference method, check if value in this group is in another group. Then return the difference of the two groups in a new group.
+    difference(value) {
+        let newGroup = new Group();
+        //Create new group
+        for(let i = 0; i < this.group.length; i++) {
+            if(!value.has(this.group[i])) {
                 newGroup.add(this.group[i]);
                 //Add value to new group
             }
         }
         return newGroup;
-        //Return new group
     }
-};
+
+}
+            
 
 
+
+//Code test from assignment
 let group1 = new Group();
+let group2 = new Group();
 group1.add(1);
 group1.add(2);
 group1.add(3);
+//Add values to group1
+console.log(group1);
+group2.add(2);
+group2.add(3);
+group2.add(5);
+group2.add(2);
+//Add values to group2
+console.log(group2);
 
+console.log(group1.has(5));
+console.log(group2.has(3));
+//Check if value is in group
+console.log(group1.union(group2));
+//Check union of group1 and group2
+console.log(group1.intersection(group2));
+//Check intersection of group1 and group2
+console.log(group1.difference(group2));
+//Check difference of group1 and group2
+group1.delete(1);
+//Remove value from group1
 console.log(group1);
-group1.add(1);
-console.log(group1);
+//Print group1 values after removing value.
+group2.delete(1);
+console.log(group2);
+//Print group2 values after removing value.
